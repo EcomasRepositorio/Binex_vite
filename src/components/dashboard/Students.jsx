37174e33451e92ca5e3a8d0896cacabdce47792a
jhoosy <maycolmedina366@gmail.com>
2023-12-30 01:42:27 -0500

    realice un pequeño cambio

--- src/components/dashboard/Students.jsx
+++ src/components/dashboard/Students.jsx
@@ -46,7 +46,7 @@ export const Students = () => {
   useEffect(() => {
     setIsLoading(true);
 
-    fetch("https://binex.edu.pe:5000/server/students")
+    fetch("https://binex.edu.pe:3000/server/students")
       .then((response) => response.json())
       .then((data) => {
         setData(data.results);
@@ -81,7 +81,7 @@ export const Students = () => {
   const handleAddStudent = (newStudentData) => {
     if (selectedStudent && selectedStudent.id_estudiante) {
       handleHttpAction(
-        `https://binex.edu.pe:5000/server/students/update/${selectedStudent.id_estudiante}`,
+        `https://binex.edu.pe:3000/server/students/update/${selectedStudent.id_estudiante}`,
         "PUT",
         newStudentData
       );
@@ -89,7 +89,7 @@ export const Students = () => {
       setSelectedStudent(null);
     } else {
       handleHttpAction(
-        "https://binex.edu.pe:5000/server/students/save",
+        "https://binex.edu.pe:3000/server/students/save",
         "POST",
         newStudentData
       );
@@ -137,7 +137,7 @@ export const Students = () => {
     if (!confirmDelete) return;
 
     handleHttpAction(
-      `https://binex.edu.pe:5000/server/students/delete/${studentCodigo}`,
+      `https://binex.edu.pe:3000/server/students/delete/${studentCodigo}`,
       "DELETE"
     );
 
@@ -165,7 +165,7 @@ export const Students = () => {
   
         // Enviar la solicitud HTTP al backend
         handleHttpAction(
-          "https://binex.edu.pe:5000/server/students/save",
+          "https://binex.edu.pe:3000/server/students/save",
           "POST",
           student
         );
