8e1037a84f99750ca79e00160e7ae798b4b7f959
jhoosy <maycolmedina366@gmail.com>
2024-01-17 10:30:50 -0500

    arreglo de errores

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3500
+    host: 'localhost', // Especifica el host como localhost
+    port: 3500,        // Especifica el puerto como 3500
   },
   exclude: [
-    '**/.git/**',  // Excluir el directorio .git y sus subdirectorios
-    '**/*.git',    // Excluir cualquier archivo o directorio con el nombre .git
-    '**/node_modules/**',  // Excluir el directorio node_modules y sus subdirectorios
+    '**/.git/**',
+    '**/*.git',
+    '**/node_modules/**',
   ],
-})
+});
