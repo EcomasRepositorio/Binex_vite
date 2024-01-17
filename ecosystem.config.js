b6707e0f26ed88dbc61e27454db1479cc5728a56
jhoosy <maycolmedina366@gmail.com>
2024-01-17 12:06:14 -0500

    arreglo de errores

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,16 @@
-module.exports = {
+export default {
   apps: [
     {
-      name: 'frontend_vite',
+      name: 'frotend_vitee',
       script: 'npm',
       args: 'run dev',
       watch: true,
       ignore_watch: ['node_modules', 'dist'], // Ignora la carpeta node_modules y dist
       env: {
         NODE_ENV: 'development',
-        PORT: 3500, // Agrega el puerto que quieres utilizar en desarrollo
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 3000, // Puedes especificar un puerto diferente para producción si es necesario
       },
     },
   ],
