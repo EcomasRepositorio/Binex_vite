270d3a4aee27b65fd40b9686821a126635b8a0ed
jhoosy <maycolmedina366@gmail.com>
2024-01-17 11:27:25 -0500

    arreglo de errores

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,17 +1,19 @@
 module.exports = {
-    apps: [
-      {
-        name: 'mi_proyecto_vite',
-        script: 'npm',
-        args: 'run dev',
-        watch: true,
-        ignore_watch: ['node_modules', 'dist'], // Ignora la carpeta node_modules y dist
-        env: {
-          NODE_ENV: 'development',
-        },
-        env_production: {
-          NODE_ENV: 'production',
-        },
+  apps: [
+    {
+      name: 'frontend_vite',
+      script: 'npm',
+      args: 'run dev',
+      watch: true,
+      ignore_watch: ['node_modules', 'dist'], // Ignora la carpeta node_modules y dist
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3500, // Agrega el puerto que quieres utilizar en desarrollo
       },
-    ],
-  };
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000, // Puedes especificar un puerto diferente para producción si es necesario
+      },
+    },
+  ],
+};
